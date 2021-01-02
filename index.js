6af4e7e876070e06b15e79f819febfb1a18d8865
unknown <ITS-Yancy@ITS-Yancy.ollh.ph>
2021-01-02 23:48:30 +0800

    Added MySQL Connection

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
-const app = express();
+const mysql = require('mysql');
 
+const app = express();
 app.use(express.json());
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: 'root', 
+    password: 'root',
+    database: 'node_api'
+});
 
 const courses = [
     {id: 1, name: "Course 1"},
@@ -14,6 +23,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/course', (req, res) => {
+    console.log('waka');
     res.send(courses);
 });
 
@@ -24,14 +34,34 @@ app.get('/api/course/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    };
-    courses.push(course);
-    console.log(courses);
-    res.send(course);
+    let sql = `INSERT INTO courses(description) VALUES ('${req.body.name}')`;
+    const result = runMysql(sql);
+    if (result.err) res.status(500).send(result.err);
+    console.log(sql);
+    res.send('Course recorded successfully');
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+// Run MySQL Command Function
+function runMysql(sql) {
+    var response = {
+        result: '',
+        err:  ''
+    };
+    console.log(typeof connection);
+    connection.connect((err) => {
+        if (err) response['err'] = 'Unable to connect to database';
+    });
+
+    connection.query(sql, (err, result) => {
+        if (err) {
+            response['err'] = err.message
+            return '';
+        }
+        response['result'] = result;
+    });
+
+    return response;
+}
